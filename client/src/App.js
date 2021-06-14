132ba1e734ce0be5b2c17a9e97e1ce3444046558
Umang Barthwal <umangbarthwal@Umangs-MacBook-Pro.local>
2021-06-14 23:40:45 +0530

    connection client-express success

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { token } from 'morgan';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Token:undefined
+    };
+  }
+
+  componentDidMount(){
+    this.getResponse();
+  }
+
+  getResponse = () => {
+    fetch("/users")
+      .then(res => res.text())
+      .then(Token => this.setState({Token}));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.Token}
+      </div>
+    );
+  }
 }
 
 export default App;
